316c9a775da3645d8d5df7bef7b1f559c93b3c51
SusimaraRoberti <susimararoberti@outlook.com>
2022-11-22 21:52:59 -0300

    fix: settings

--- CNAB/settings.py
+++ CNAB/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-67%i)r#b&nvdl%2_#!cw*+5aioc&%&c#v&if=u=wmk_4_z9m1p"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
